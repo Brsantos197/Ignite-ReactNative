17212059c0fb13587d000229518e1de0f12daee4
Bruno Santos <brsantos197@gmail.com>
2022-10-27 01:00:28 -0300

    up

---
+++ igniteteams/src/components/Highlight/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, Title, Subtitle } from './styles';
+
+type Props = {
+  title: string;
+  subtitle: string;
+}
+
+export const Highlight = ({ title, subtitle }: Props) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+    </Container>
+  )
+}
+
